4858cc3c65a28f06dbcbee3950ca623d81ca778d
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-06-29 01:04:21 +0800

    Major Changes on Feature
    - Implemented new API (CoinAPI)
    - Implemented Recent Currency List
    - Dynamic Currency Listing

--- calcrypto.js
+++ calcrypto.js
@@ -1,16 +1,81 @@
+const key = "53C9B0CF-E34D-4EFC-8079-6A9A259ABA2F";
+const fiatCurrency = ["USD", "AUD", "CNY", "EUR", "GBP", "JPY", "MYR", "SGD"];
+const CryptoCurrency = ["ADA", "AXS", "BNB", "BTC", "DOGE", "ETH", "SHIB", "SLP", "SOL", "XRP"];
+const recentInputCurrency = [];
+const recentOutputCurrency = [];
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    var inputFiatList = document.getElementById("input-fiat");
+    var outputFiatList = document.getElementById("output-fiat");
+
+    fiatCurrency.forEach(element => {
+        inputFiatList.innerHTML += "<option value=" + element + ">" + element + "</option>";
+        outputFiatList.innerHTML += "<option value=" + element + ">" + element + "</option>";
+    });
+});
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    var inputCryptoList = document.getElementById("input-crypto");
+    var outputCryptoList = document.getElementById("output-crypto");
+
+    CryptoCurrency.forEach(element => {
+        inputCryptoList.innerHTML += "<option value=" + element + ">" + element + "</option>";
+        outputCryptoList.innerHTML += "<option value=" + element + ">" + element + "</option>";
+    });
+});
+
+function updateRecent() {
+    var inputRecentList = document.getElementById("input-recent");
+    var outputRecentList = document.getElementById("output-recent");
+
+    while (inputRecentList.firstChild) inputRecentList.removeChild(inputRecentList.firstChild);
+    while (outputRecentList.firstChild) outputRecentList.removeChild(outputRecentList.firstChild);
+
+    recentInputCurrency.forEach(element => {
+        inputRecentList.innerHTML += "<option value=" + element + ">" + element + "</option>";
+    });
+
+    recentOutputCurrency.forEach(element => {
+        outputRecentList.innerHTML += "<option value=" + element + ">" + element + "</option>";
+    });
+}
+
 document.getElementById("btnConvert").onclick = function() {
-    const base = document.getElementById('input-currency').value;
-    fetch(`https://api.exchangerate.host/latest?/source=ecb&base=${base}`)
+    const inputCurrency = document.getElementById('input-currency').value;
+    const outputCurrency = document.getElementById('output-currency').value;
+    fetch(`https://rest.coinapi.io/v1/exchangerate/${inputCurrency}/${outputCurrency}?apikey=${key}`)
         .then((response) => response.json())
         .then((data) => {
             // console.log(data)
             const amount = document.getElementById("input-amount").value;
-            const currencyTo = document.getElementById('output-currency').value;
-            const rate = data.rates[currencyTo];
+            const rate = data.rate;
+
             function convert(){
                 return amount * rate;
             }
             document.getElementById("output-amount").value = convert();
+
+            if (recentInputCurrency.length === 0) {
+                recentInputCurrency.push(inputCurrency);
+            } else {
+                var flagExist = false;
+                for (let i = 0; i < recentInputCurrency.length; i++ ) {
+                    if (recentInputCurrency[i] == inputCurrency) flagExist = true;
+                }
+                if (!flagExist) recentInputCurrency.push(inputCurrency);
+            }
+
+            if (recentOutputCurrency.length === 0) {
+                recentOutputCurrency.push(outputCurrency);
+            } else {
+                var flagExist = false;
+                for (let i = 0; i < recentOutputCurrency.length; i++ ) {
+                    if (recentOutputCurrency[i] == outputCurrency) flagExist = true;
+                }
+                if (!flagExist) recentOutputCurrency.push(outputCurrency);
+            }
+
+            updateRecent();
         })
         .catch((error) =>{
             console.log("Error: ", error);
